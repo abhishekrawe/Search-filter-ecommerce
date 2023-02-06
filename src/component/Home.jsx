77f02b4481f5216ba751ce77b0a8c508475b15fe
Abhishek Rawe <65603830+abhishekrawe@users.noreply.github.com>
2023-02-06 20:30:23 +0530

    😎Updated Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -33,13 +33,6 @@ function Home() {
 
         return (
             <>
-                {/* <div className='buttons d-flex justify-content-center mb-5 pb-5'>
-                    <button className='btn btn-outline-dark me-2' onClick={() => setFilter(data)}>All</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("jewelery")}>Jewelery</button>
-                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("electronics")}>Electronics</button>
-                </div> */}
                 {filter.map((product) => {
                     return (
                         <>
